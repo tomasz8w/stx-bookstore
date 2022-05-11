e9b38006fa16613e7291318bb0791bd29e134b70
tomasz8w <td.wieclaw@gmail.com>
2022-05-11 08:48:56 +0200

    SearchBar form handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import BookList from "./components/BookList";
 import ContentWrapper from "./components/ContentWrapper";
 import HeroSection from "./components/HeroSection";
 import SearchBar from "./components/SearchBar";
 
 function App() {
+  const onSearch = (searchText: string, selectedLang: string) => {
+    console.log(searchText);
+  };
+
   return (
     <ContentWrapper>
       <HeroSection />
-      <SearchBar />
+      <SearchBar onSearch={onSearch} />
+      <BookList />
     </ContentWrapper>
   );
 }
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,33 +1,74 @@
-const SearchBar = () => (
-  <section
-    style={{
-      display: "flex",
-      flexDirection: "row",
-      borderColor: "gray",
-      gap: "1rem",
-      boxShadow: "1px 1px 3px rgb(0,0,0,0.16)",
-      borderRadius: "5px",
-      alignItems: "center",
-    }}
-  >
-    <input type="search" />
-    <input type="radio" id="pl" value="pl" name="lang" />
-    <label htmlFor="pl">PL</label>
-    <input type="radio" id="en" value="en" name="lang" />
-    <label htmlFor="en">EN</label>
+import React, { useState } from "react";
 
-    <button
-      type="button"
+type Props = {
+  onSearch: (searchText: string, selectedLang: string) => void;
+};
+
+const SearchBar = ({ onSearch }: Props) => {
+  const [searchText, setSearchText] = useState("");
+  const [selectedLang, setSelectedLang] = useState("pl");
+
+  const handleSearchTextChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setSearchText(event.currentTarget.value);
+  };
+
+  const handleLangChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setSelectedLang(event.currentTarget.value);
+  };
+
+  const handleSearchClick = () => {
+    onSearch(searchText, selectedLang);
+  };
+
+  return (
+    <section
       style={{
-        border: "1px solid gray",
-        backgroundColor: "#fff",
-        padding: "0.4rem",
+        display: "flex",
+        flexDirection: "row",
+        borderColor: "gray",
+        gap: "1rem",
+        boxShadow: "1px 1px 3px rgb(0,0,0,0.16)",
         borderRadius: "5px",
+        alignItems: "center",
+        padding: "0.5rem",
       }}
     >
-      Search
-    </button>
-  </section>
-);
+      <input
+        type="search"
+        value={searchText}
+        onChange={handleSearchTextChange}
+      />
+      <input
+        type="radio"
+        id="pl"
+        value="pl"
+        name="lang"
+        defaultChecked
+        onChange={handleLangChange}
+      />
+      <label htmlFor="pl">PL</label>
+      <input
+        type="radio"
+        id="en"
+        value="en"
+        name="lang"
+        onChange={handleLangChange}
+      />
+      <label htmlFor="en">EN</label>
 
+      <button
+        type="button"
+        onClick={handleSearchClick}
+        style={{
+          border: "1px solid gray",
+          backgroundColor: "#fff",
+          padding: "0.4rem",
+          borderRadius: "5px",
+        }}
+      >
+        Search
+      </button>
+    </section>
+  );
+};
 export default SearchBar;
